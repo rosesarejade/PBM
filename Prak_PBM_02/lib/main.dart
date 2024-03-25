c17d95e5b0ddf396148d76a59e1c545ce4daa74d
Faiqa Hadya Hanifa <142599518+rosesarejade@users.noreply.github.com>
2024-03-25 11:29:19 +0700

    Update main.dart to my code

--- Prak_PBM_02/lib/main.dart
+++ Prak_PBM_02/lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text(appTitle),
         ),
-        // #docregion addWidget
         body: const SingleChildScrollView(
           child: Column(
             children: [
@@ -62,11 +61,11 @@ class TitleSection extends StatelessWidget {
       child: Row(
         children: [
           Expanded(
-            /*1*/
+            //step 1
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                /*2*/
+                //step break
                 Padding(
                   padding: const EdgeInsets.only(bottom: 8),
                   child: Text(
@@ -85,13 +84,12 @@ class TitleSection extends StatelessWidget {
               ],
             ),
           ),
-          /*3*/
-          // #docregion Icon
+          // step break
+          //Icon
           Icon(
             Icons.star,
             color: Colors.red[500],
           ),
-          // #enddocregion Icon
           const Text('41'),
         ],
       ),
@@ -185,7 +183,6 @@ class TextSection extends StatelessWidget {
   }
 }
 
-// #docregion ImageSection
 class ImageSection extends StatelessWidget {
   const ImageSection({super.key, required this.image});
 
@@ -193,76 +190,12 @@ class ImageSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // #docregion Image-asset
+    // Image asset
     return Image.asset(
       image,
       width: 600,
       height: 240,
       fit: BoxFit.cover,
     );
-    // #enddocregion Image-asset
   }
-}
-// #enddocregion ImageSection
-
-// #docregion FavoriteWidget
-class FavoriteWidget extends StatefulWidget {
-  const FavoriteWidget({super.key});
-
-  @override
-  State<FavoriteWidget> createState() => _FavoriteWidgetState();
-}
-// #enddocregion FavoriteWidget
-
-// #docregion _FavoriteWidgetState, _FavoriteWidgetState-fields, _FavoriteWidgetState-build
-class _FavoriteWidgetState extends State<FavoriteWidget> {
-  // #enddocregion _FavoriteWidgetState-build
-  bool _isFavorited = true;
-  int _favoriteCount = 41;
-
-  // #enddocregion _FavoriteWidgetState-fields
-
-  // #docregion _toggleFavorite
-  void _toggleFavorite() {
-    setState(() {
-      if (_isFavorited) {
-        _favoriteCount -= 1;
-        _isFavorited = false;
-      } else {
-        _favoriteCount += 1;
-        _isFavorited = true;
-      }
-    });
-  }
-
-  // #enddocregion _toggleFavorite
-
-  // #docregion _FavoriteWidgetState-build
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Container(
-          padding: const EdgeInsets.all(0),
-          child: IconButton(
-            padding: const EdgeInsets.all(0),
-            alignment: Alignment.centerRight,
-            icon: (_isFavorited
-                ? const Icon(Icons.star)
-                : const Icon(Icons.star_border)),
-            color: Colors.red[500],
-            onPressed: _toggleFavorite,
-          ),
-        ),
-        SizedBox(
-          width: 18,
-          child: SizedBox(
-            child: Text('$_favoriteCount'),
-          ),
-        ),
-      ],
-    );
-  }
-// #docregion _FavoriteWidgetState-fields
 }
